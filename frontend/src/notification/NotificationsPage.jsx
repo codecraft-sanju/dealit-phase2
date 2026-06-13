ed86db30326ed6a73f1fb045932c22e8ef2008ba
codecraft-sanju <sanjusirvich18@gmail.com>
2026-06-13 18:09:17 +0530

    hdhdhhd

--- frontend/src/notification/NotificationsPage.jsx
+++ frontend/src/notification/NotificationsPage.jsx
@@ -365,7 +365,7 @@ const NotificationsPage = () => {
         </div>
       </header>
 
-      <main className="flex-1 flex flex-col pt-24 pb-24 px-4 relative z-10 max-w-md mx-auto w-full">
+      <main className="flex-1 flex flex-col pt-18 pb-24 px-4 relative z-10 max-w-md mx-auto w-full">
         
         <div className="flex overflow-x-auto hide-scrollbar gap-2 mb-4 py-2 -mx-4 px-4 sticky top-0 z-20 bg-[#f4f2f9]/90 backdrop-blur-sm">
           {['All', 'Trades', 'Wallet', 'Orders'].map((filter) => (
